d0230cf052779e9be93a8385b328f2d54a71fc9e
real-tea <akash.0711a@gmail.com>
2023-01-09 13:04:11 +0530

    webrtc ice servers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useRef , useState, useEffect } from "React";
 import './App.css';
 import download from 'downloadjs';
-
+import { generateUsername } from "unique-username-generator";
 
 
 function App() {
@@ -10,7 +10,40 @@ function App() {
   const [peerUsername , setPeerUsername] = useState("");
   const userList = useRef([""]);
 
+  const production = false // TODO : CHANGE IT TO TRUE WHEN DEPLOYING
 
+
+  var configuration = {
+    iceServers : [
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun1.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:19302" },
+    ]
+  }
+
+  const baseURL = production 
+  ? `https://${window.location.hostname}`
+  : "https://192.168.18.27:3000";
+
+  var peerConnection = useRef( new RTCPeerConnection(configuration) );
+
+
+  useEffect(()=>{
+    const userName = generateUsername("-");
+    window.document.title = "SEMD";
+    if(window.location.pathname !== "/"){
+      let p = window.location.pathname.slice(1);
+      myName.current = p;
+      const btn: any = docoment.querySelectorAll(".btn");
+      btn.forach((i:any)=>{
+        i.classList.add("hidden")
+      });
+
+      const searc
+    }
+  })
 
   return (
     <div >
